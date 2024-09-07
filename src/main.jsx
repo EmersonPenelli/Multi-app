4073bc0bc88371811d5af6693bd93a9da61c3d4d
EmersonPenelli <emersonpenelli@gmail.com>
2024-09-07 19:08:20 -0300

    fix: correções de bugs de importações

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter as Router } from 'react-router-dom'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
-<<<<<<< HEAD
 import { AuthProvider } from './components/Contexts/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -13,13 +12,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <App />
       </Router>
     </AuthProvider>
-=======
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
->>>>>>> a15729691886a11a600d094604c998dad4a4dcda
   </React.StrictMode>,
 )
